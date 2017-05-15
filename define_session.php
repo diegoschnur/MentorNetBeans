32e0e3ce01d5f4379edbd875bf40b55ed87e7d57
diegoschnur <diegoschnur@gmail.com>
2017-05-15 20:10:51 -0300

    Add files via upload

---
+++ define_session.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+( isset($_POST['variavel']) ? $variavel = $_POST['variavel'] : $variavel = "" );
+( isset($_POST['valor']) ? $valor = $_POST['valor'] : $valor = "" );
+if( $variavel != "" ){
+//    if( $_SESSION[$variavel] != $valor ){
+        $_SESSION[$variavel] = $valor;
+//    }
+}
+header("Location: " . $_POST['destino']);
